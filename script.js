bd05be7bbc040cfe3172f91b8165a194deb55460
RyanD177 <w0477770@campus.nscc.ca>
2023-07-05 03:11:11 -0300

    responsive website practice

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-const hamburger = document.querySelector(".nav__hamburger");
-const linksContainer  = document.querySelector(".nav__menu");
-const links = document.querySelectorAll(".nav__menu__link"); 
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
-hamburger.addEventListener("click", () => {
-  linksContainer.classList.toggle("active");
-  hamburger.classList.toggle("active");
-})
-window.addEventListener("resize", () => {
-  if(window.matchMedia("(max-width: 550px)").matches){
-    closeMenu();
-  }
-})
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a [href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
 
+/* Toggle */
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-if(window.matchMedia("(max-width: 550px)").matches){
-  closeMenu();
-}
-
-function closeMenu () { 
-  links.forEach((link => {
-    link.addEventListener("click", () => {
-      linksContainer.classList.remove("active");
-      hamburger.classList.remove("active");
-    })
-  }))
-
-}
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
